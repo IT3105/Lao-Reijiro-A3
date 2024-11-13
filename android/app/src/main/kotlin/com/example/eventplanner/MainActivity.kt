d96e516b027850c988de4df37b86a9134a72a654
isoreij <22102327@usc.edu.ph>
2024-11-13 21:24:07 +0800

    android

--- android/app/src/main/kotlin/com/example/eventplanner/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/eventplanner/MainActivity.kt
@@ -1,5 +1,37 @@
+// android/app/src/main/kotlin/com/example/eventplanner/MainActivity.kt
 package com.example.eventplanner
 
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.os.Bundle
+import android.widget.RemoteViews
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "com.example.eventplanner/widget"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "updateEventWidget") {
+                val events: String? = call.argument("events")
+                if (events != null) {
+                    updateWidget(events)
+                }
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun updateWidget(events: String) {
+        val appWidgetManager = AppWidgetManager.getInstance(this)
+        val views = RemoteViews(packageName, R.layout.event_widget)
+        views.setTextViewText(R.id.widget_event_list, events)
+
+        val widgetComponent = ComponentName(this, EventWidgetProvider::class.java)
+        appWidgetManager.updateAppWidget(widgetComponent, views)
+    }
+}
